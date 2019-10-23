db499bc98816b57bd3ee2ec8ab2823aeacd9cb90
schabertdowneyjr <sschab689@gmail.com>
2019-10-23 15:52:50 -0600

    added third solution

--- function-drills-1.js
+++ function-drills-1.js
@@ -9,3 +9,12 @@ function createGreeting() {
 }
 
 createGreeting();
+
+//3
+function createGreeting() {
+    return 'Hi, my name is Stephen and I\'m 33 years old';
+}
+
+let greeting = createGreeting();
+
+console.log(greeting);
