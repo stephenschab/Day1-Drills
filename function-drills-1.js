2d752fec547c8f0cd99ecb1a2d6a073acede61a2
schabertdowneyjr <sschab689@gmail.com>
2019-10-23 15:49:16 -0600

    added second solution

--- function-drills-1.js
+++ function-drills-1.js
@@ -2,3 +2,10 @@
 
 //1
 console.log('Hi, my name is Stephen and I\'m 33 years old');
+
+//2
+function createGreeting() {
+    console.log('Hi, my name is Stephen and I\'m 33 years old');
+}
+
+createGreeting();
